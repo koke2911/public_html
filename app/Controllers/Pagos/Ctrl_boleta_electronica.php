49d76484a301ba26c6bc8e193c85651697af6974
victor <victor.martinez@saludohiggins.cl>
2025-05-08 07:38:34 -0400

    boletas ultimo folio

--- app/Controllers/Pagos/Ctrl_boleta_electronica.php
+++ app/Controllers/Pagos/Ctrl_boleta_electronica.php
@@ -1830,16 +1830,24 @@ public function emitir_dte_new(){
 
     $datosMetros = $this->db->query("SELECT ifnull(max(folio_bolect),0) as maximo from metros m inner join arranques a on a.id_socio=m.id_socio where m.id_apr=$id_apr and url_boleta is not null and a.id_tipo_documento=1")->getRow();
     $datosMetros39 = $this->db->query("SELECT ifnull(max(folio_bolect),0) as maximo from metros m inner join arranques a on a.id_socio=m.id_socio where m.id_apr=$id_apr and url_boleta is not null and a.id_tipo_documento=3")->getRow();
+
+    $datosMetros34 = $this->db->query("SELECT ifnull(max(folio_bolect),0) as maximo from metros m inner join arranques a on a.id_socio=m.id_socio where m.id_apr=$id_apr and url_boleta is not null and a.id_tipo_documento=2")->getRow();
+    $datosMetros33 = $this->db->query("SELECT ifnull(max(folio_bolect),0) as maximo from metros m inner join arranques a on a.id_socio=m.id_socio where m.id_apr=$id_apr and url_boleta is not null and a.id_tipo_documento=4")->getRow();
     
 
     // print_r($datosMetros->maximo);
 
     $ultimo= $datosMetros->maximo;
     $ultimo39 = $datosMetros39->maximo;
+
+    $ultimo33 = $datosMetros33->maximo;
+    $ultimo34 = $datosMetros34->maximo;
    
     $datosAPR     = [
      'ultimo_folio'=>$ultimo,
      'ultimo_folio_afecta'=>$ultimo39,
+      'ultimo_folio_fe' => $ultimo34,
+      'ultimo_folio_fa' => $ultimo33,
      'id'=>$id_apr
     ];
 
